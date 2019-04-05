55aa9ac1e3ce5957ea6ebb6df62854530b266e0f
Jerry Kuo <jerrykuo820@gmail.com>
2019-04-05 01:54:35 -0700

    Initial Commit

---
+++ StringUtils.cpp
@@ -0,0 +1,67 @@
+#include "StringUtils.h"
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <cstdio>
+
+namespace StringUtils {
+
+    std::string Slice(const std::string &str, ssize_t start, ssize_t end) {
+        // Your code goes here
+        std::string temp;
+        std::cout << start;
+    }
+
+    std::string Capitalize(const std::string &str) {
+        // Your code goes here
+    }
+
+    std::string Title(const std::string &str) {
+        // Your code goes here
+    }
+
+    std::string LStrip(const std::string &str) {
+        // Your code goes here
+    }
+
+    std::string RStrip(const std::string &str) {
+        // Your code goes here
+    }
+
+    std::string Strip(const std::string &str) {
+        // Your code goes here
+    }
+
+    std::string Center(const std::string &str, int width, char fill) {
+        // Your code goes here
+    }
+
+    std::string LJust(const std::string &str, int width, char fill) {
+        // Your code goes here
+    }
+
+    std::string RJust(const std::string &str, int width, char fill) {
+        // Your code goes here
+    }
+
+    std::string Replace(const std::string &str, const std::string &old, const std::string &rep) {
+        // Your code goes here
+    }
+
+    std::vector<std::string> Split(const std::string &str, const std::string &splt) {
+        // Your code goes here
+    }
+
+    std::string Join(const std::string &str, const std::vector<std::string> &vect) {
+        // Your code goes here
+    }
+
+    std::string ExpandTabs(const std::string &str, int tabsize) {
+        // Your code goes here
+    }
+
+    int EditDistance(const std::string &left, const std::string &right, bool ignorecase) {
+        // Your code goes here
+    }
+
+}
