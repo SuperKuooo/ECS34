fa65ea49248d955a39331262e75c81ca11379b10
SuperKuo <jerrykuo820@gmail.com>
2019-04-06 03:26:19 -0700

    Adding String Utils Functions

--- StringUtils.cpp
+++ StringUtils.cpp
@@ -7,49 +7,165 @@
 namespace StringUtils {
 
     std::string Slice(const std::string &str, ssize_t start, ssize_t end) {
-        // Your code goes here
         std::string temp;
-        std::cout << start;
+        int counter = 0;
+        int length = str.length();
+        if (start < 0) {
+            start += length;
+        }
+        if (end <= 0) {
+            end += length;
+        }
+        for (char ch : str) {
+            if (counter < start) {
+                counter++;
+                continue;
+            } else if (start <= counter && counter < end) {
+                counter++;
+                temp += ch;
+            } else {
+                break;
+            }
+        }
+        return temp;
     }
 
     std::string Capitalize(const std::string &str) {
-        // Your code goes here
+        std::string temp;
+        for (char ch : str) {
+            temp += tolower(ch);
+        }
+        temp[0] = toupper(temp[0]);
+        return temp;
     }
 
     std::string Title(const std::string &str) {
-        // Your code goes here
+        std::string temp, lower_case;
+        char prev;
+        bool first = true;
+        // First do a little bit a input manipulation.
+        for (char ch : str) {
+            lower_case += tolower(ch);
+        }
+        for (char ch: lower_case) {
+            if (first) {
+                first = false;
+                prev = ch;
+                temp += toupper(ch);
+                continue;
+            }
+            if (ispunct(prev) or prev == ' ' or !isalpha(prev)) {
+                prev = ch;
+                temp += toupper(ch);
+                continue;
+            }
+            temp += ch;
+            prev = ch;
+        }
+        return temp;
     }
 
     std::string LStrip(const std::string &str) {
-        // Your code goes here
+        std::string temp;
+        bool stripped = true;
+        for (char ch : str) {
+            if (isblank(ch) and stripped) {
+                continue;
+            } else {
+                stripped = false;
+                temp += ch;
+            }
+        }
+        return temp;
     }
 
     std::string RStrip(const std::string &str) {
-        // Your code goes here
+        std::string temp;
+        int length = str.length() - 1;
+        temp = str;
+        for (int counter = length; counter >= 0; counter--) {
+            if (isblank(temp[counter])) {
+                temp.erase(counter, 1);
+            } else {
+                break;
+            }
+        }
+        return temp;
     }
 
     std::string Strip(const std::string &str) {
-        // Your code goes here
+        std::string temp;
+        temp = LStrip(str);
+        temp = RStrip(temp);
+        return temp;
     }
 
     std::string Center(const std::string &str, int width, char fill) {
-        // Your code goes here
+        std::string temp;
+        temp = Strip(str);
+        int length = temp.length() + 1;
+        if ((width - temp.length()) % 2) {
+            temp += fill;
+        }
+        for (int counter = length; counter < width; counter += 2) {
+            temp += fill;
+            temp = fill + temp;
+        }
+        return temp;
     }
 
     std::string LJust(const std::string &str, int width, char fill) {
-        // Your code goes here
+        std::string temp;
+        temp = Strip(str);
+        int length = temp.length();
+        for (int counter = length; counter < width; counter++) {
+            temp += fill;
+        }
+        return temp;
     }
 
     std::string RJust(const std::string &str, int width, char fill) {
-        // Your code goes here
+        std::string temp, just;
+        temp = Strip(str);
+        int length = temp.length();
+        for (int counter = length; counter < width; counter++) {
+            temp = fill + temp;
+        }
+
+        return temp;
     }
 
     std::string Replace(const std::string &str, const std::string &old, const std::string &rep) {
-        // Your code goes here
+        std::string temp;
+        int replace = str.find(old);
+        temp = str;
+        while (replace != std::string::npos) {
+            temp.replace(replace, rep.length(), rep);
+            replace = temp.find(old);
+            // Inspired by https://stackoverflow.com/questions/15740829/string-find-doesnt-return-1b
+        }
+        return temp;
     }
 
     std::vector<std::string> Split(const std::string &str, const std::string &splt) {
-        // Your code goes here
+        std::vector<std::string> x;
+        std::string temp;
+
+        int div = str.find(splt), div2;
+        temp = str;
+        while (true) {
+            temp = &temp[div+1];
+            div2 = temp.find(splt);
+            if (div2 != std::string::npos) {
+                x.push_back(Slice(temp, 0, div2));
+                std::cout << x.size();
+                div = div2;
+            } else{
+                x.push_back(temp);
+                break;
+            }
+        }
+        return x;
     }
 
     std::string Join(const std::string &str, const std::vector<std::string> &vect) {
--- main.cpp
+++ main.cpp
@@ -2,68 +2,68 @@
 #include "StringUtils.h"
 #include "Path.h"
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     bool Done = false;
-    do{
+    do {
         std::string CommandString;
-        std::cout<<std::endl<<"1 - Title String"<<std::endl;
-        std::cout<<"2 - Compare Strings"<<std::endl;
-        std::cout<<"3 - Absolute Path"<<std::endl;
-        std::cout<<"4 - Relative Path"<<std::endl;
-        std::cout<<"-----------------"<<std::endl;
-        std::cout<<"Q - Quit"<<std::endl;
-        std::cout<<"> ";
-        
-        std::getline(std::cin, CommandString);
-        if(!std::cin.fail()){
-            // CommandString = StringUtils::Strip(CommandString);
-            if(CommandString == "1"){
-                std::string TitleString;
-                std::cout<<"Enter string> ";
-                std::getline(std::cin, TitleString);
-                printf("Hey");
-                StringUtils::Slice(TitleString, 1, 3);
+        std::cout << std::endl << "1 - Title String" << std::endl;
+        std::cout << "2 - Compare Strings" << std::endl;
+        std::cout << "3 - Absolute Path" << std::endl;
+        std::cout << "4 - Relative Path" << std::endl;
+        std::cout << "-----------------" << std::endl;
+        std::cout << "Q - Quit" << std::endl;
+        std::cout << "> ";
 
+        std::getline(std::cin, CommandString);
+        if (!std::cin.fail()) {
+            // CommandString = StringUtils::Strip(CommandString);
+            if (CommandString == "1") {
+                //std::string TitleString;
+                //std::cout << "Enter string> ";
+                //std::getline(std::cin, TitleString);
+                std::vector<std::string> x;
+                x = StringUtils::Split("/Path/To/A/Directory","/");
+                std::cout << x.size();
+                std::cout << x[0];
                 /*
                 if(!std::cin.fail()){
                     std::cout<<"|"<<StringUtils::Center(std::string(" ") + StringUtils::Strip(TitleString) + std::string(" "),77,'*')<<"|"<<std::endl;   
                 }*/
-            }
-            else if(CommandString == "2"){
+            } else if (CommandString == "2") {
                 std::string String1, String2;
-                std::cout<<"Enter 1st string> ";
+                std::cout << "Enter 1st string> ";
                 std::getline(std::cin, String1);
-                if(!std::cin.fail()){
-                    std::cout<<"Enter 2nd string> ";
+                if (!std::cin.fail()) {
+                    std::cout << "Enter 2nd string> ";
                     std::getline(std::cin, String2);
-                    if(!std::cin.fail()){
-                        std::cout<<"\""<<String1<<"\" requires "<<StringUtils::EditDistance(String1, String2)<<" edits to become \""<<String2<<"\""<<std::endl;
+                    if (!std::cin.fail()) {
+                        std::cout << "\"" << String1 << "\" requires " << StringUtils::EditDistance(String1, String2)
+                                  << " edits to become \"" << String2 << "\"" << std::endl;
                     }
                 }
-            }
-            else if(CommandString == "3"){
+            } else if (CommandString == "3") {
                 std::string Path;
-                std::cout<<"Enter a path> ";
+                std::cout << "Enter a path> ";
                 std::getline(std::cin, Path);
-                if(!std::cin.fail()){
-                    std::cout<<"The absolute path of \""<<Path<<"\" is \""<<std::string(CPath::AbsolutePath(CPath(Path)))<<"\""<<std::endl;
+                if (!std::cin.fail()) {
+                    std::cout << "The absolute path of \"" << Path << "\" is \""
+                              << std::string(CPath::AbsolutePath(CPath(Path))) << "\"" << std::endl;
                 }
-            }
-            else if(CommandString == "4"){
+            } else if (CommandString == "4") {
                 std::string Path;
-                std::cout<<"Enter a path> ";
+                std::cout << "Enter a path> ";
                 std::getline(std::cin, Path);
-                if(!std::cin.fail()){
-                    std::cout<<"The relative path of \""<<Path<<"\" is \""<<std::string(CPath::RelativePath(CPath(Path)))<<"\""<<std::endl;
+                if (!std::cin.fail()) {
+                    std::cout << "The relative path of \"" << Path << "\" is \""
+                              << std::string(CPath::RelativePath(CPath(Path))) << "\"" << std::endl;
                 }
-            }
-            else if((CommandString == "Q") or (CommandString == "q")){
+            } else if ((CommandString == "Q") or (CommandString == "q")) {
                 Done = true;
             }
         }
-        
-    }while(!Done);
-    
-    
+
+    } while (!Done);
+
+
     return EXIT_SUCCESS;
 }
