449c6f635dc3a03f8f78388b8ff447e578b1f3fc
charissa8606 <charissa8606@gmail.com>
2019-05-30 12:40:03 -0700

    discussion code
    not complete, missed some code, need to fill in some blanks

---
+++ MapRouter.h
@@ -0,0 +1,55 @@
+#ifndef MAPROUTER_H
+#define MAPROUTER_H
+
+#include <vector>
+#include <istream>
+#include <map>
+
+class CMapRouter{
+    public:
+        using TNodeID = unsigned long;
+        using TStopID = unsigned long;
+        using TLocation = std::pair<double, double>;
+        using TPathStep = std::pair<std::string, TNodeID>;
+        
+        static const TNodeID InvalidNodeID;
+    private:
+        using TNodeIndex = int;
+        struct Edge{
+            TNodeIndex OtherNode;
+            double Distance;
+            double Time;
+        };
+        struct Node{
+            TNodeID NodeID;
+            TLocation Location;
+            std::vector< Edge > Edges;
+        };
+
+        std::vector< Node > Nodes;
+        std::map< TNodeID, ???> ???;
+        std::vector< TNodeID > SortedIDs;
+        
+    public:
+        CMapRouter();
+        ~CMapRouter();
+        
+        static double HaversineDistance(double lat1, double lon1, double lat2, double lon2);
+        static double CalculateBearing(double lat1, double lon1,double lat2, double lon2);
+        
+        bool LoadMapAndRoutes(std::istream &osm, std::istream &stops, std::istream &routes);
+        size_t NodeCount() const;
+        TNodeID GetSortedNodeIDByIndex(size_t index) const;
+        TLocation GetSortedNodeLocationByIndex(size_t index) const;
+        TLocation GetNodeLocationByID(TNodeID nodeid) const;
+        TNodeID GetNodeIDByStopID(TStopID stopid) const;
+        size_t RouteCount() const;
+        std::string GetSortedRouteNameByIndex(size_t index) const;
+        bool GetRouteStopsByRouteName(const std::string &route, std::vector< TStopID > &stops);
+        
+        double FindShortestPath(TNodeID src, TNodeID dest, std::vector< TNodeID > &path);
+        double FindFastestPath(TNodeID src, TNodeID dest, std::vector< TPathStep > &path);
+        bool GetPathDescription(const std::vector< TPathStep > &path, std::vector< std::string > &desc) const;
+};
+
+#endif
